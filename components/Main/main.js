5bc1ef18b60b8f305b52d94fd032081437b6f4fe
Jamie Waters <jwaters627@gmail.com>
2017-11-19 10:53:55 -0500

    put data into a data.js file and pull it into Main.js file with component will mount and wrote function to organize the data into rows

--- components/Main/main.js
+++ components/Main/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './main.scss';
 
+import Data from '../../data';
+
 
 export default class Main extends React.Component {
 	 constructor(props) {
@@ -10,6 +12,24 @@ export default class Main extends React.Component {
 	    };
     }
 
+    componentWillMount(){
+		this.organizeRows()
+	}
+
+	// Organize the data into rows by going through each seat object and creating a key in the plane object for each row and have its value be an array of each seat in that row.
+
+	organizeRows = () =>{
+		let plane = {};
+		Data.map(function(seat){
+			if(plane[seat.row]){
+				plane[seat.row].push(seat)
+			}
+			else{
+				plane[seat.row] = [seat];
+			}
+		})
+		console.log(plane)
+	}
 
 	render() {
 		return(
